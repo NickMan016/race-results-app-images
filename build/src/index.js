a94f0fca4b9253e437ef6de9af6c65c4ba3f1246
NickMan016 <npcastillo73@gmail.com>
2023-06-20 13:27:15 -0600

    Generando Build para Railway

---
+++ build/src/index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const drivers_1 = __importDefault(require("./routes/drivers"));
+const express_fileupload_1 = __importDefault(require("express-fileupload"));
+const PORT = 3000;
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, express_fileupload_1.default)());
+app.get('/test', (_, res) => {
+    res.send('App running');
+});
+app.use('/api/drivers', drivers_1.default);
+app.listen(PORT, () => {
+    console.log(`App running in port ${PORT}`);
+});
