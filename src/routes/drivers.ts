40471e7e9a4e36fdfebe596939e7e4c577a805d3
Nicholas Pérez <nicolas.perez@spiralis.com.mx>
2024-02-17 22:19:45 -0600

    Actualización para almacenar datos e imágenes

--- src/routes/drivers.ts
+++ src/routes/drivers.ts
@@ -1,17 +1,41 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs-extra';
-import path from 'path';
+// import path from 'path';
 import * as driverServices from './../services/driverServices';
+import {v2 as cloudinary} from 'cloudinary';
+import { Driver } from '../types';
+          
+cloudinary.config({ 
+  cloud_name: 'dpjnmd62g', 
+  api_key: '415735428159965', 
+  api_secret: 'Z3t_Aj_1PIjQ2V4owi6D2ocqMEg' 
+});
 
 const router = express.Router();
 
-router.get('/', (_, res) => {
-    res.send(driverServices.getDrivers())
+router.get('/', async (_, res) => {
+    try {
+        const drivers = await driverServices.getDrivers();
+        res.send(drivers);
+    } catch (error) {
+        // Manejar el error
+        res.status(500).send('Error al obtener conductores');
+    }
 });
 
-router.get('/:id', (req, res) => {
-    res.send(driverServices.getDriverById(req.params.id))
+router.get('/:id', async (req, res) => {
+    try {
+        const driver = await driverServices.getDriverById(req.params.id);
+
+        if (driver) {
+            res.send(driver);
+        } else {
+            res.status(404).send('Conductor no encontrado');
+        }
+    } catch (error) {
+        res.status(500).send('Error al obtener conductor por id');
+    }
 });
 
 router.post('/', async (req, res) => {
@@ -26,22 +50,31 @@ router.post('/', async (req, res) => {
             await fs.ensureDir(`${__dirname}/../../uploads/drivers`);
 
             if (id === 'default') {
-                await file.mv(`${__dirname}/../../uploads/drivers/Default.jpg`, (err: any) => {
+                const filePath = `${__dirname}/../../uploads/drivers/Default.jpg`;
+                await file.mv(filePath, async (err: any) => {
                     if (err) {
                         res.json(`No se pudo subir el archivo, ${err}`);
                     } else {
+                        const response = await cloudinary.uploader.upload(filePath);
+                        driverServices.addDriver({
+                            id,
+                            image: response.secure_url
+                        });
+                        
                         res.json('Se ha subido el archivo Default');
                     }
                 });
             } else {
                 const image = `${uuidv4()}.${file.mimetype.split('/')[1]}`;
-                await file.mv(`${__dirname}/../../uploads/drivers/${image}`, (err: any) => {
+                const filePath = `${__dirname}/../../uploads/drivers/${image}`;
+                await file.mv(filePath, async (err: any) => {
                     if (err) {
                         res.json(`No se pudo subir el archivo, ${err}`);
                     } else {
+                        const response = await cloudinary.uploader.upload(filePath);
                         const newDriver = driverServices.addDriver({
                             id,
-                            image
+                            image: response.secure_url
                         });
 
                         res.json(newDriver);
@@ -55,50 +88,62 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.put('/', async (req, res) => {
-    const { id } = req.body;
+// router.put('/', async (req, res) => {
+//     const { id } = req.body;
 
-    if (!req.files || Object.keys(req.files).length === 0) {
-        res.json('No se subió un archivo');
-    } else {
-        const file: any = req.files.image;
+//     if (!req.files || Object.keys(req.files).length === 0) {
+//         res.json('No se subió un archivo');
+//     } else {
+//         const file: any = req.files.image;
 
-        if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
-            const image = `${uuidv4()}.${file.mimetype.split('/')[1]}`;
-            const driver = driverServices.getDriverById(id);
+//         if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+//             const image = `${uuidv4()}.${file.mimetype.split('/')[1]}`;
+//             const driver = driverServices.getDriverById(id);
 
-            await fs.ensureDir(`${__dirname}/../../uploads/drivers`);
-            await file.mv(`${__dirname}/../../uploads/drivers/${image}`, (err: any) => {
-                if (err) {
-                    res.json(`No se pudo subir el archivo, ${err}`);
-                } else {
-                    fs.unlink(`${__dirname}/../../uploads/drivers/${driver?.image}`);
-                    const updateDriver = driverServices.updateDriver({
-                        id,
-                        image
-                    });
+//             await fs.ensureDir(`${__dirname}/../../uploads/drivers`);
+//             await file.mv(`${__dirname}/../../uploads/drivers/${image}`, (err: any) => {
+//                 if (err) {
+//                     res.json(`No se pudo subir el archivo, ${err}`);
+//                 } else {
+//                     fs.unlink(`${__dirname}/../../uploads/drivers/${driver?.image}`);
+//                     const updateDriver = driverServices.updateDriver({
+//                         id,
+//                         image
+//                     });
 
-                    res.json(updateDriver);
-                }
-            });
+//                     res.json(updateDriver);
+//                 }
+//             });
 
+//         } else {
+//             res.json('El archivo no es una imagen');
+//         }
+//     }
+// });
+
+router.get('/:id/image', async (req, res) => {
+    try {
+        const driver = await driverServices.getDriverById(req.params.id);
+
+        if (driver) {
+            res.redirect(driver.image);
         } else {
-            res.json('El archivo no es una imagen');
+            const def = await driverServices.getDriverById('default') as Driver;
+            res.redirect(def.image);
         }
+    } catch (error) {
+        res.status(500).send('Error al obtener conductor por id');
     }
-});
+    // const { id } = req.params;
+    // const imageDriver = driverServices.getImage(id);
+    // const pathImage = path.resolve(__dirname, `../../uploads/drivers/${imageDriver}`);
 
-router.get('/:id/image', (req, res) => {
-    const { id } = req.params;
-    const imageDriver = driverServices.getImage(id);
-    const pathImage = path.resolve(__dirname, `../../uploads/drivers/${imageDriver}`);
-
-    if (fs.existsSync(pathImage)) {
-        res.sendFile(pathImage);
-    } else {
-        const pathNoImage = path.resolve(__dirname, '../../uploads/drivers/Default.jpg');
-        res.sendFile(pathNoImage);
-    }
+    // if (fs.existsSync(pathImage)) {
+    //     res.sendFile(pathImage);
+    // } else {
+    //     const pathNoImage = path.resolve(__dirname, '../../uploads/drivers/Default.jpg');
+    //     res.sendFile(pathNoImage);
+    // }
 });
 
 export default router;
--- src/services/driverServices.ts
+++ src/services/driverServices.ts
@@ -1,6 +1,16 @@
 import path from 'path';
 import { Driver } from '../types';
 import fs from 'fs-extra';
+import admin, { ServiceAccount } from 'firebase-admin';
+
+import serviceAccount from "../../race-results-api-images-firebase-adminsdk-uym0i-e5471ed4c7.json";
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    databaseURL: 'https://race-results-api-images-default-rtdb.firebaseio.com/'
+});
+
+const db = admin.database();
 
 const pathJSON = path.resolve(__dirname, './drivers.json');
 let data:any = [];
@@ -13,10 +23,33 @@ if (!fs.existsSync(pathJSON)) {
 
 let drivers: Driver[] = data as Driver[];
 
-export const getDrivers = (): Driver[] => drivers;
+export const getDrivers = async (): Promise<Driver[]> => {
+    const snapshot = await db.ref('drivers').once('value');
+    const drivers: Driver[] = snapshot.val();
+    return drivers;
+};
 
-export const getDriverById = (id: string): Driver | undefined => {
-    return drivers.find(d => d.id === id);
+export const getDriverById = async (id: string): Promise<Driver | null> => {
+    try {
+        const driversRef = db.ref('drivers');
+
+        // Realiza la consulta para obtener el objeto con el id específico
+        const snapshot = await driversRef.orderByChild('id').equalTo(id).once('value');
+        // Verifica si hay datos en el resultado
+        if (snapshot.exists()) {
+            const driverKey = Object.keys(snapshot.val())[0];
+            // Obtiene el primer hijo (debería ser único porque estamos buscando por id)
+            const driver = snapshot.val()[driverKey] as Driver;
+            return driver;
+        } else {
+            return null; // No se encontró ningún conductor con el id especificado
+        }
+    } catch (error) {
+        // console.error('Error:', error);
+        // Manejar el error
+        throw error;
+    }
+    // return drivers.find(d => d.id === id);
 }
 
 export const addDriver = (newDriverEntry: Driver): Driver => {
@@ -24,8 +57,9 @@ export const addDriver = (newDriverEntry: Driver): Driver => {
         ...newDriverEntry
     }
 
-    drivers.push(newDriver);
-    fs.writeJSONSync(`${ __dirname }/drivers.json`, drivers);
+    db.ref('drivers').push(newDriver);
+    // drivers.push(newDriver);
+    // fs.writeJSONSync(`${ __dirname }/drivers.json`, drivers);
     return newDriver;
 }
 
