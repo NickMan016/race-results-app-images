571f0cf9bc489cd768bca284d8debb0821c6db55
Nicholas Pérez <npcastillo73@gmail.com>
2024-04-02 23:17:19 -0600

    Utilización de Variable de Entorno para borrar json de Firebase

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,26 @@
 import admin, { ServiceAccount } from "firebase-admin";
+import dotenv from 'dotenv';
 
-import serviceAccount from "../race-results-api-images-firebase-adminsdk-uym0i-e5471ed4c7.json";
+dotenv.config();
+
+const clientEmail = process.env.CLIENT_EMAIL;
+const privateKey = process.env.PRIVATE_KEY;
+const projectId = process.env.PROJECT_ID;
+
+if (!clientEmail || !privateKey || !projectId) {
+  console.error("Faltan algunas variables de entorno necesarias.");
+  process.exit(1);
+}
+
+const serviceAccount: ServiceAccount = {
+  clientEmail,
+  privateKey,
+  projectId
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
-  databaseURL: "https://race-results-api-images-default-rtdb.firebaseio.com/",
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DATABASE_URL,
 });
 
 export const db = admin.database();
